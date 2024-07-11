11ce7db5095fd82115b97ca6843d6079115f7fc6
pkaodev <polsson.development@gmail.com>
2024-07-11 22:36:45 +0100

    wah

--- README.md
+++
@@ -1,4 +0,0 @@
-# Portfolio site for PKAO.dev
-
-Made using the startbootstrap-creative template:
-https://github.com/startbootstrap/startbootstrap-creative
--- script.js
+++ script.js
@@ -1,112 +1,182 @@
-document.addEventListener('DOMContentLoaded', () => {
-	const animated_div = document.querySelector('.animated_div');
-	const globalStyle = document.createElement('style');
-	document.head.appendChild(globalStyle);
+class Quadtree {
+    constructor(boundBox, lvl = 0) {
+        this.boundBox = boundBox;
+        this.level = lvl;
+        this.maxLevel = 5;
+        this.maxObjects = 10;
+        this.objects = [];
+        this.nodes = [];
+    }
 
-	const CSS_COLORS = ['4a', '5a', '6a', '7a', '8a', '9a', '10a', '4b', '5b', '6b', '7b', '8b', '9b', '10b', '4c', '5c', '6c', '7c', '8c', '9c', '10c', '4z', '5z', '6z', '7z', '8z', '9z', '10z'];
-	const LETTERS = "UVWXYZabcdefg%^&*()ABCDEFcdefg%^&*()_+{GHIJKLMNOPQ23456789!@#$%^&*()_RSTUVWXYZabcdefg%^&*()_+{}|:<>?~`-ABCDEFGHIJKLMNOPQ23456789!@#=[]\;',./hijklmnopqHIJKLMNOPQRSTUrstuvwxyz0123456789!@#$%^&*()_+{}|:bcdefg%^&*()_+{<>?~`-=[]\;',./";
+    split() {
+        const subWidth = this.boundBox.width / 2;
+        const subHeight = this.boundBox.height / 2;
+        const x = this.boundBox.x;
+        const y = this.boundBox.y;
 
-	const NUM_FALLING_ITEMS = 25;
+        this.nodes[0] = new Quadtree({x: x + subWidth, y: y, width: subWidth, height: subHeight}, this.level + 1);
+        this.nodes[1] = new Quadtree({x: x, y: y, width: subWidth, height: subHeight}, this.level + 1);
+        this.nodes[2] = new Quadtree({x: x, y: y + subHeight, width: subWidth, height: subHeight}, this.level + 1);
+        this.nodes[3] = new Quadtree({x: x + subWidth, y: y + subHeight, width: subWidth, height: subHeight}, this.level + 1);
+    }
+
+    getIndex(rect) {
+        const verticalMidpoint = this.boundBox.x + (this.boundBox.width / 2);
+        const horizontalMidpoint = this.boundBox.y + (this.boundBox.height / 2);
+        const topQuadrant = (rect.y < horizontalMidpoint && rect.y + rect.height < horizontalMidpoint);
+        const bottomQuadrant = (rect.y > horizontalMidpoint);
+
+        if (rect.x < verticalMidpoint && rect.x + rect.width < verticalMidpoint) {
+            if (topQuadrant) {
+                return 1;
+            } else if (bottomQuadrant) {
+                return 2;
+            }
+        } else if (rect.x > verticalMidpoint) {
+            if (topQuadrant) {
+                return 0;
+            } else if (bottomQuadrant) {
+                return 3;
+            }
+        }
+        return -1;
+    }
+
+    insert(rect) {
+        if (this.nodes[0] != null) {
+            const index = this.getIndex(rect);
+
+            if (index !== -1) {
+                this.nodes[index].insert(rect);
+                return;
+            }
+        }
+
+        this.objects.push(rect);
+
+        if (this.objects.length > this.maxObjects && this.level < this.maxLevel) {
+            if (this.nodes[0] == null) {
+                this.split();
+            }
+
+            let i = 0;
+            while (i < this.objects.length) {
+                const index = this.getIndex(this.objects[i]);
+                if (index !== -1) {
+                    this.nodes[index].insert(this.objects.splice(i, 1)[0]);
+                } else {
+                    i++;
+                }
+            }
+        }
+    }
+
+    retrieve(returnObjects, rect) {
+        const index = this.getIndex(rect);
+        if (index !== -1 && this.nodes[0] != null) {
+            this.nodes[index].retrieve(returnObjects, rect);
+        }
+
+        returnObjects.push(...this.objects);
+    }
+
+    clear() {
+        this.objects = [];
+
+        for (let i = 0; i < this.nodes.length; i++) {
+            if (this.nodes[i] != null) {
+                this.nodes[i].clear();
+                this.nodes[i] = null;
+            }
+        }
+    }
+}
+
+const squares = [];
+const tree = new Quadtree({ x: 0, y: 0, width: window.innerWidth, height: window.innerHeight });
+const square_class_styles = ['sqr_1', 'sqr_2', 'sqr_3', 'sqr_4', 'sqr_5', 'sqr_6', 'sqr_7', 'sqr_8', 'sqr_9', 'sqr_10', 'sqr_11', 'sqr_12', 'sqr_13', 'sqr_14', 'sqr_15']; 
+document.addEventListener('click', function(e) {
+    if (e.button === 0) {
+        createSquare(e.clientX, e.clientY);
+    }
+});
+function change_square_class(square) {
+    const currentClass = square.element.classList.value.split(' ').find(cls => square_class_styles.includes(cls));
+    const currentIndex = square_class_styles.indexOf(currentClass);
+    const nextIndex = (currentIndex + 1) % square_class_styles.length;
+    const nextClass = square_class_styles[nextIndex];
+
+    // Update the class on the square
+    square.element.classList.remove(currentClass);
+    square.element.classList.add(nextClass);
+}
+
+function createSquare(x, y) {
+
+    const square = document.createElement('div');
+
+	square.classList.add('sqr_1');
+
+	const width = 50;
+	const height = 50;
+	square.style.width = width + 'px';
+	square.style.height = height + 'px';
+
+	const dx = 2;
+	const dy = 2;
 
 	
+	square.style.left = x + 'px';
+	square.style.top = y + 'px';
 
-	function createFallingContainer() {
-		const fallingContainer = document.createElement('div');
-		animated_div.appendChild(fallingContainer);
-		const textContainer = document.createElement('div');
-		fallingContainer.appendChild(textContainer);
-
-		const { width: WIDTH_NUM, height: HEIGHT_NUM } = _TEXTBOX_CHAR_SIZE();
-		const { width: WIDTH_CHAR, height: HEIGHT_CHAR, fontSize: FONT_SIZE, lineHeight: LINE_HEIGHT } = _CHAR_PIXEL_SIZE();
-		const { BOX_WIDTH, BOX_HEIGHT } = _TEXTBOX_PIXEL_SIZE(WIDTH_NUM, HEIGHT_NUM, WIDTH_CHAR, HEIGHT_CHAR);
-
-		console.log(BOX_WIDTH, BOX_HEIGHT);
-		console.log(WIDTH_NUM, HEIGHT_NUM, WIDTH_CHAR, HEIGHT_CHAR, FONT_SIZE, LINE_HEIGHT);
-		
-		const HORIZONTAL_SPEED = _HORIZONTAL_SPEED();
-
-		fallingContainer.style.position = 'absolute';
-		fallingContainer.style.left =  _LEFT();
-
-		const fadeAnimationKeyframes = [
-			{ opacity: 0.8, offset: 0 },
-			{ opacity: 0.8, offset: 0.5 },
-			{ opacity: 0.2, offset: 0.8 },
-			{ opacity: 0, offset: 0.9 },
-			{ opacity: 0, offset: 1 }
-		];
-		const fadeAnimationOptions = {
-			duration: _VERTICAL_DURATION(),
-			iterations: Infinity,
-			easing: 'linear'
-		};
-
-		const fallingAnimationKeyframes = [
-			{ top: `${BOX_HEIGHT / 4}px`, offset: 0 },
-			{ top: `calc(100vh + ${BOX_HEIGHT}px)`, offset: 0.9 },
-			{ top: `calc(100vh + ${BOX_HEIGHT}px)`, offset: 1 }
-		];
-		const fallingAnimationOptions = {
-			duration: _VERTICAL_DURATION(),
-			iterations: Infinity,
-			easing: 'linear'
-		};
-		fallingContainer.animate(fallingAnimationKeyframes, fallingAnimationOptions);
-
-		for (let j = 0; j < HEIGHT_NUM; j++) {
-			const singleLetter = document.createElement('div');
-			textContainer.appendChild(singleLetter);
-			singleLetter.animate(fadeAnimationKeyframes, fadeAnimationOptions);
-
-			singleLetter.style.position = 'absolute';
-			singleLetter.style.left = '0';
-
-			singleLetter.style.fontSize =  FONT_SIZE + 'px';
-			singleLetter.style.lineHeight = LINE_HEIGHT;
-			singleLetter.style.top = `-${(j * HEIGHT_CHAR)}px`;
-			singleLetter.style.color = `var(--color-${CSS_COLORS[Math.floor(Math.random() * CSS_COLORS.length)]})`;
-			singleLetter.style.width = `${WIDTH_NUM * WIDTH_CHAR}px`;
-
-			let i_letters = Math.floor(Math.random() * LETTERS.length);
-			setInterval(() => {
-				i_letters = (i_letters + 1) % LETTERS.length;
-				singleLetter.innerHTML = LETTERS.slice(i_letters, i_letters + WIDTH_NUM);
-				singleLetter.left = _LEFT();
-			}, HORIZONTAL_SPEED);
-		}
-
-	}
-
-	for (let i = 0; i < NUM_FALLING_ITEMS; i++) {
-		setTimeout(createFallingContainer, 150 * i);
-	}
-});
-
-function _TEXTBOX_CHAR_SIZE() {
-	let height = Math.floor(Math.random() * 80) + 15;
-	let width = Math.floor(Math.random() * height / 3) + 1;
-	return { height, width };
-}
-function _CHAR_PIXEL_SIZE() {
-	let width = Math.floor(Math.random() * 20) + 10;
-	let fontSize = Math.floor(Math.random() * 20) + 10;
-	let lineHeight = 1.2;
-	let height = fontSize * lineHeight;
-	return { width, height, fontSize, lineHeight };
-}
-function _TEXTBOX_PIXEL_SIZE(WIDTH_NUM, HEIGHT_NUM, WIDTH_CHAR, HEIGHT_CHAR) {
-	BOX_WIDTH = WIDTH_NUM * WIDTH_CHAR;
-	BOX_HEIGHT = HEIGHT_NUM * HEIGHT_CHAR;
-	return { BOX_WIDTH, BOX_HEIGHT };
+    document.body.appendChild(square);
+    squares.push({ element: square, x: x, y: y, dx: dx, dy: dy, width: width, height: height });
 }
 
-function _VERTICAL_DURATION() {
-	return Math.floor(Math.random() * 25000) + 10000;
-}
-function _HORIZONTAL_SPEED() {
-	return Math.floor(Math.random() * 150) + 150;
+function moveSquares() {
+    tree.clear();
+    squares.forEach(square => {
+        tree.insert(square);
+    });
+
+    squares.forEach(square => {
+        let possibleCollisions = [];
+        tree.retrieve(possibleCollisions, square);
+
+        for (let other of possibleCollisions) {
+            if (other !== square && isColliding(square, other)) {
+                square.dx = -square.dx;
+                square.dy = -square.dy;
+                other.dx = -other.dx;
+                other.dy = -other.dy;
+                change_square_class(square);
+            }
+        }
+
+        square.x += square.dx;
+        square.y += square.dy;
+
+        if (square.x <= 0 || square.x + square.width >= window.innerWidth) {
+            change_square_class(square);
+            square.dx = -square.dx;
+        }
+        if (square.y <= 0 || square.y + square.height >= window.innerHeight) {
+            change_square_class(square);
+            square.dy = -square.dy;
+        }
+
+        square.element.style.left = square.x + 'px';
+        square.element.style.top = square.y + 'px';
+    });
+
+    requestAnimationFrame(moveSquares);
 }
 
-function _LEFT() {
-	return Math.floor(Math.random() * 100) + '%';
+function isColliding(rect1, rect2) {
+    return !(rect1.x + rect1.width < rect2.x || 
+             rect1.x > rect2.x + rect2.width || 
+             rect1.y + rect1.height < rect2.y || 
+             rect1.y > rect2.y + rect2.height);
 }
+
+moveSquares();
